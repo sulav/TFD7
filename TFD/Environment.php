c70537ad1a81435c62ebfef0701052672d4e1864
Rene Bakx <rene.bakx@ara.nl>
2015-07-31 13:13:24 +0200

    Fixed issue with template referencing like a sub theme but without a sub theme
    {% include 'mytheme::includes/include.tpl.twig' %} would only work if mytheme was a base theme from the currently active theme.
    
    This caused problems if the base theme is used as a active theme.

--- TFD/Environment.php
+++ TFD/Environment.php
@@ -1,110 +1,112 @@
 <?php
-/*
-* This file is part of Twig For Drupal 7.
+
+/* Extended environmnent for the drupal version
 *
-* @see http://tfd7.rocks for more information
-*
-* @author René Bakx
-* Extended environmnent for the drupal version
+* Part of the Drupal twig extension distribution
+* http://renebakx.nl/twig-for-drupal
 */
 
-class TFD_Environment extends Twig_Environment {
+class TFD_Environment extends Twig_Environment
+{
 
-  protected $templateClassPrefix = '__TFDTemplate_';
-  protected $fileExtension = 'tpl.twig';
-  protected $autoRender = FALSE;
+    protected $templateClassPrefix = '__TFDTemplate_';
+    protected $fileExtension = 'tpl.twig';
+    protected $autoRender = false;
 
-  public function __construct(Twig_LoaderInterface $loader = NULL, $options = array()) {
-    $this->fileExtension = twig_extension();
-    $options = array_merge(array(
-      'autorender' => TRUE,
-    ), $options);
-    // Auto render means, overrule default class
-    if ($options['autorender']) {
-      $this->autoRender = TRUE;
-    }
-    parent::__construct($loader, $options);
-  }
-
-  public function isAutoRender() {
-    return $this->autoRender;
-  }
-
-  /**
-   * returns the name of the class to be created
-   * which is also the name of the cached instance
-   *
-   * @param <string> $name of template
-   * @return <string>
-   */
-  public function getTemplateClass($name, $index = NULL) {
-    return str_replace(array(
-      '-',
-      '.',
-      '/'
-    ), "_", $this->generateCacheKeyByName($name));
-  }
-
-  private function generateCacheKeyByName($name) {
-    return $name = preg_replace('/\.' . $this->fileExtension . '$/', '', $this->loader->getCacheKey($name));
-  }
-
-  public function loadTemplate($name, $index = NULL) {
-
-    if (substr_count($name, '::') == 1) {
-      $paths = twig_get_discovered_templates(); // Very expensive call
-      $name = $paths[$name];
-    }
-
-    return parent::loadTemplate($name, $index);
-  }
-
-  public function getCacheFilename($name) {
-    if ($cache = $this->getCache()) {
-      $name = $this->generateCacheKeyByName($name);
-      $name .= '.php';
-      $dir = $cache . '/' . dirname($name);
-      if (!is_dir($dir)) {
-        if (!mkdir($dir, 0777, TRUE)) {
-          throw new Exception("Cache directory $cache is not deep writable?");
+    public function __construct(Twig_LoaderInterface $loader = null, $options = array())
+    {
+        $this->fileExtension = twig_extension();
+        $options = array_merge(array(
+            'autorender' => true,
+        ), $options);
+        // Auto render means, overrule default class
+        if ($options['autorender']) {
+            $this->autoRender = true;
         }
-      }
-      return $cache . '/' . $name;
+        parent::__construct($loader, $options);
     }
-  }
 
-
-  public function flushCompilerCache() {
-    if (is_dir($this->getCache())) {
-      $iterator = new RecursiveDirectoryIterator($this->getCache());
-      foreach (new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $file) {
-        if ($file->isDir()) {
-          @rmdir($file->getPathname());
-        }
-        else {
-          @unlink($file->getPathname());
-        }
-      }
-      @rmdir($this->getCache());
+    private function generateCacheKeyByName($name)
+    {
+        return $name = preg_replace('/\.' . $this->fileExtension . '$/', '', $this->loader->getCacheKey($name));
     }
-  }
 
-
-  protected function writeCacheFile($file, $content) {
-    try {
-      if (!is_dir(dirname($file))) {
-        mkdir(dirname($file), 0777, TRUE);
-      }
-      $tmpFile = tempnam(dirname($file), basename($file));
-      if (FALSE !== @file_put_contents($tmpFile, $content)) {
-        // rename does not work on Win32 before 5.2.6
-        if (@rename($tmpFile, $file) || (@copy($tmpFile, $file) && unlink($tmpFile))) {
-          @chmod($file, 0644);
-          // Just in case apc.stat = 0, force the cache file into APC cache!
-        }
-      }
-    } catch (Exception $exception) {
-      throw new Twig_Error_Runtime(sprintf('Failed to write cache file "%s".', $file));
+    public function isAutoRender()
+    {
+        return $this->autoRender;
     }
-  }
+
+    /**
+     * returns the name of the class to be created
+     * which is also the name of the cached instance
+     *
+     * @param <string> $name of template
+     * @return <string>
+     */
+    public function getTemplateClass($name, $index = null)
+    {
+        return str_replace(array('-', '.', '/'), "_", $this->generateCacheKeyByName($name));
+    }
+
+    public function loadTemplate($name, $index = null)
+    {
+
+        if (substr_count($name, '::') == 1) {
+            $paths = twig_get_discovered_templates(); // Very expensive call
+            $name = $paths[$name];
+        }
+
+        return parent::loadTemplate($name, $index);
+    }
+
+    public function getCacheFilename($name)
+    {
+        if ($cache = $this->getCache()) {
+            $name = $this->generateCacheKeyByName($name);
+            $name .= '.php';
+            $dir = $cache . '/' . dirname($name);
+            if (!is_dir($dir)) {
+                if (!mkdir($dir, 0777, true)) {
+                    throw new Exception("Cache directory $cache is not deep writable?");
+                }
+            }
+            return $cache . '/' . $name;
+        }
+    }
+
+
+    public function flushCompilerCache()
+    {
+        if (is_dir($this->getCache())) {
+            $iterator = new RecursiveDirectoryIterator($this->getCache());
+            foreach (new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $file) {
+                if ($file->isDir()) {
+                    @rmdir($file->getPathname());
+                } else {
+                    @unlink($file->getPathname());
+                }
+            }
+            @rmdir($this->getCache());
+        }
+    }
+
+
+    protected function writeCacheFile($file, $content)
+    {
+        try {
+            if (!is_dir(dirname($file))) {
+                mkdir(dirname($file), 0777, true);
+            }
+            $tmpFile = tempnam(dirname($file), basename($file));
+            if (false !== @file_put_contents($tmpFile, $content)) {
+                // rename does not work on Win32 before 5.2.6
+                if (@rename($tmpFile, $file) || (@copy($tmpFile, $file) && unlink($tmpFile))) {
+                    @chmod($file, 0644);
+                    // Just in case apc.stat = 0, force the cache file into APC cache!
+                }
+            }
+        } catch (Exception $exception) {
+            throw new Twig_Error_Runtime(sprintf('Failed to write cache file "%s".', $file));
+        }
+    }
 }
--- TFD/Loader/Filesystem.php
+++ TFD/Loader/Filesystem.php
@@ -1,51 +1,51 @@
 <?php
-/*
- * This file is part of Twig For Drupal 7.
+
+/**
+ * Loads template from the filesystem.
  *
- * @author René Bakx
- * @see http://tfd7.rocks for more information
- *
- * @description Loads template from the filesystem.
+ * Part of the Drupal twig extension distribution
+ * http://renebakx.nl/twig-for-drupal
  */
-class TFD_Loader_Filesystem extends Twig_Loader_Filesystem {
-  protected $resolverCache;
+class TFD_Loader_Filesystem extends Twig_Loader_Filesystem
+{
+    protected $resolverCache;
 
-  public function __construct() {
-    parent::__construct(array());
-    $this->resolverCache = array();
-  }
-
-
-  public function getSource($filename) {
-    return file_get_contents($this->getCacheKey($filename));
-  }
-
-
-  public function findTemplate($name) {
-    $this->validateName($name);
-    if (!isset($this->resolverCache[$name])) {
-      $found = FALSE;
-      if (is_readable($name)) {
-        $this->resolverCache[$name] = $name;
-        $found = TRUE;
-      }
-      else {
-        $paths = twig_get_discovered_templates();
-
-        if (array_key_exists($name, $paths)) {
-          $completeName = $paths[$name];
-          if (is_readable($completeName)) {
-            $this->resolverCache[$name] = $completeName;
-            $found = TRUE;
-          }
-        }
-      }
-      if (!$found) {
-        throw new Twig_Error_Loader(sprintf('Could not find a cache key for template "%s"', $name));
-      }
+    public function __construct()
+    {
+        parent::__construct(array());
+        $this->resolverCache = array();
     }
-    return $this->resolverCache[$name];
-  }
+
+
+    public function getSource($filename)
+    {
+        return file_get_contents($this->getCacheKey($filename));
+    }
+
+
+    public function findTemplate($name)
+    {
+        $this->validateName($name);
+        if (!isset($this->resolverCache[$name])) {
+            $found = false;
+            if (is_readable($name)) {
+                $this->resolverCache[$name] = $name;
+                $found = true;
+            } else {
+                $paths = twig_get_discovered_templates();
+
+                if (array_key_exists($name, $paths)) {
+                    $completeName = $paths[$name];
+                    if (is_readable($completeName)) {
+                        $this->resolverCache[$name] = $completeName;
+                        $found = true;
+                    }
+                }
+            }
+            if (!$found) throw new Twig_Error_Loader(sprintf('Could not find a cache key for template "%s"', $name));
+        }
+        return $this->resolverCache[$name];
+    }
 
 }
 
